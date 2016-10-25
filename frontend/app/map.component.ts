a6e83d108e626cdb57b94b7cbe6927bce2dc5888
Mo Nuaimat <nuaimat@gmail.com>
2016-10-25 15:24:49 -0500

    add basic template binding

--- frontend/app/map.component.ts
+++ frontend/app/map.component.ts
@@ -6,6 +6,15 @@ import {Component} from '@angular/core';
 })
 export class MapComponent {
     title: string = 'Job Posting'
-    lat: number = 51.678418;
-    lng: number = 7.809007;
+    lat: number = 41.00641495699017;
+    lng: number = -91.96061968803406; // location[0]
+    zoom: number = 18;
+    disableDoubleClickZoom: boolean = true;
+    scrollwheel :boolean = false;
+    zoomControl: boolean = true;
+
+
+    showAddJobDialog(event) {
+        console.log(event);
+    }
 }
